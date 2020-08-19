b851ce4c7ca0724974109596939283145ad20bf5
Emmanuel Plumas <eplumas@free.fr>
2020-08-19 17:56:51 +0200

    CReation du projet p7JavaLibrarEasy-Batch

---
+++ p7JavaLibrarEasy-Batch/src/main/java/com/emmanuel/plumas/p7JavaLibrarEasyBatch/P7JavaLibrarEasyBatchApplication.java
@@ -0,0 +1,13 @@
+package com.emmanuel.plumas.p7JavaLibrarEasyBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P7JavaLibrarEasyBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P7JavaLibrarEasyBatchApplication.class, args);
+	}
+
+}
---
+++ p7JavaLibrarEasy-Batch/src/test/java/com/emmanuel/plumas/p7JavaLibrarEasyBatch/P7JavaLibrarEasyBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emmanuel.plumas.p7JavaLibrarEasyBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P7JavaLibrarEasyBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
