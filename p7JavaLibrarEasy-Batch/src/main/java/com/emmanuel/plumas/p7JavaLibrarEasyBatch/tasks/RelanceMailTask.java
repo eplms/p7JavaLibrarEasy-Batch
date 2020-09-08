79e5f5ffd3e6f14c98c3d029eced709fe2ff2b97
Emmanuel Plumas <eplumas@free.fr>
2020-09-08 10:08:03 +0200

    Amélioration du contenu du mail de relance
    Prise en compte du nom et du prénom en plus du titre de l'ouvrage.

--- p7JavaLibrarEasy-Batch/src/main/java/com/emmanuel/plumas/p7JavaLibrarEasyBatch/tasks/RelanceMailTask.java
+++ p7JavaLibrarEasy-Batch/src/main/java/com/emmanuel/plumas/p7JavaLibrarEasyBatch/tasks/RelanceMailTask.java
@@ -1,8 +1,6 @@
 package com.emmanuel.plumas.p7JavaLibrarEasyBatch.tasks;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Properties;
 
 import javax.mail.Authenticator;
@@ -17,7 +15,6 @@ import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 
-import java.util.HashMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -37,8 +34,8 @@ public class RelanceMailTask {
 	
 	public void execute() throws MessagingException {
 		// ApiProxy Récuperer la liste des adresses mail des utilisateurs dont le prêt est arrivé à son terme
-		Map<String,String> userEntitiesMailandBook = getOutOfTimeBorrow();
-		if(userEntitiesMailandBook.size()!=0) {
+		List<BorrowEntity> borrowEntities=apiProxy.getOutOfTimeAndNotReturnedBorrow();
+		if(borrowEntities.size()!=0) {
 				
 			//Paramètres d'initialisation
 			Properties prop = new Properties();
@@ -54,25 +51,30 @@ public class RelanceMailTask {
 			        return new PasswordAuthentication("epika@free.fr", "PETIL33");
 			    }
 				});
-			for(Entry<String, String> userEntityMailAndBookEntry : userEntitiesMailandBook.entrySet()) {
+			for(BorrowEntity borrowEntity : borrowEntities) {
 				//Création et envoi du message
 				Message message = new MimeMessage(session);
 	
 				
 				message.setFrom(new InternetAddress("epika@free.fr"));
 				message.setRecipients(
-				  Message.RecipientType.TO, InternetAddress.parse(userEntityMailAndBookEntry.getKey()));
+				  Message.RecipientType.TO, InternetAddress.parse(borrowEntity.getUserEntity().getUserEmail()));
 				message.setSubject("Votre emprunt de livre");
 				 
 				String newLine=System.lineSeparator();
-				String line1 = "Bonjour,"+newLine;
+				String line1 = "Bonjour ";
 				String line2= "Vous avez emprunté à la bibliothèque l'ouvrage suivant : "+newLine;
 				String line3= newLine+". Merci de bien vouloir le rapporter dans les plus brefs délais. D'autres ouvrages sont à découvrir avec chaque mois des nouveautés."+newLine;
 				String line4="Au plaisir de vour revoir dans nos rayons."+newLine;
 				String line5="Toute l'équipe de la bibliothèque";
 	
 				MimeBodyPart mimeBodyPart = new MimeBodyPart();
-				mimeBodyPart.setContent(line1+line2+(userEntityMailAndBookEntry.getValue())+line3+line4+line5, "text/html");
+				mimeBodyPart.setContent(line1
+						+borrowEntity.getUserEntity().getUserFirstName()+" "
+						+borrowEntity.getUserEntity().getUserLastName()+". "
+						+line2
+						+borrowEntity.getCopyEntity().getBookEntity().getBookTitle()
+						+line3+line4+line5, "text/html");
 				 
 				Multipart multipart = new MimeMultipart();
 				multipart.addBodyPart(mimeBodyPart);
@@ -92,17 +94,5 @@ public class RelanceMailTask {
 			}
 		
 		}
-
-	
-	private Map<String, String> getOutOfTimeBorrow(){
-		List<BorrowEntity> borrowEntities=apiProxy.getOutOfTimeAndNotReturnedBorrow();
-		Map<String,String> userEntitiesMailandBook = new HashMap<String,String>();
-		for(BorrowEntity borrowEntity:borrowEntities) {
-			userEntitiesMailandBook.put(borrowEntity.getUserEntity().getUserEmail(),borrowEntity.getCopyEntity().getBookEntity().getBookTitle());
-		}
-		return userEntitiesMailandBook;
-	}
-	
-
 	
 }
