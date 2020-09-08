e371f67c15198dafd4d845e31c4503cd71bc58b6
Emmanuel Plumas <eplumas@free.fr>
2020-09-08 08:55:01 +0200

    Gestion du cas de l'absence de mail à envoyer.

--- p7JavaLibrarEasy-Batch/src/main/java/com/emmanuel/plumas/p7JavaLibrarEasyBatch/tasks/RelanceMailTask.java
+++ p7JavaLibrarEasy-Batch/src/main/java/com/emmanuel/plumas/p7JavaLibrarEasyBatch/tasks/RelanceMailTask.java
@@ -37,51 +37,55 @@ public class RelanceMailTask {
 	public void execute() throws MessagingException {
 		// ApiProxy Récuperer la liste des adresses mail des utilisateurs dont le prêt est arrivé à son terme
 		List<String> userEntitiesMail = getMailUserOutOfTimeBorrow();
-		
+		if(userEntitiesMail.size()!=0) {
 				
-		//Paramètres d'initialisation
-		Properties prop = new Properties();
-		prop.put("mail.smtp.auth", true);
-		prop.put("mail.smtp.host", "smtp.free.fr");
-		prop.put("mail.smtp.port", "25");
-
-
-		//Création de la session de connection avec le serveur de mail et Authentification
-		Session session = Session.getInstance(prop, new Authenticator() {
-		    @Override
-		    protected PasswordAuthentication getPasswordAuthentication() {
-		        return new PasswordAuthentication("epika@free.fr", "PETIL33");
-		    }
-		});
-
-		for(String userEntityMail :userEntitiesMail) {
-			//Création et envoi du message
-			Message message = new MimeMessage(session);
-
-			
-			message.setFrom(new InternetAddress("epika@free.fr"));
-			message.setRecipients(
-			  Message.RecipientType.TO, InternetAddress.parse(userEntityMail));
-			message.setSubject("Votre emprunt de livre");
-			 
-			String msg = "Bonjour, Merci de bien vouloir nous rendre le en votre possession."
-					+ "un autre lecteur l'attend peut-être ....";
-
-			MimeBodyPart mimeBodyPart = new MimeBodyPart();
-			mimeBodyPart.setContent(msg, "text/html");
-			 
-			Multipart multipart = new MimeMultipart();
-			multipart.addBodyPart(mimeBodyPart);
-			 
-			message.setContent(multipart);	
-			
-			try {        
-		         Transport.send(message);
-		         System.out.println("Message envoyé avec succès....");
-				} catch (MessagingException mex) {
-					mex.printStackTrace();
+			//Paramètres d'initialisation
+			Properties prop = new Properties();
+			prop.put("mail.smtp.auth", true);
+			prop.put("mail.smtp.host", "smtp.free.fr");
+			prop.put("mail.smtp.port", "25");
+	
+	
+			//Création de la session de connection avec le serveur de mail et Authentification
+			Session session = Session.getInstance(prop, new Authenticator() {
+			    @Override
+			    protected PasswordAuthentication getPasswordAuthentication() {
+			        return new PasswordAuthentication("epika@free.fr", "PETIL33");
+			    }
+				});
+	
+			for(String userEntityMail :userEntitiesMail) {
+				//Création et envoi du message
+				Message message = new MimeMessage(session);
+	
+				
+				message.setFrom(new InternetAddress("epika@free.fr"));
+				message.setRecipients(
+				  Message.RecipientType.TO, InternetAddress.parse(userEntityMail));
+				message.setSubject("Votre emprunt de livre");
+				 
+				String msg = "Bonjour, Merci de bien vouloir nous rendre le en votre possession."
+						+ "un autre lecteur l'attend peut-être ....";
+	
+				MimeBodyPart mimeBodyPart = new MimeBodyPart();
+				mimeBodyPart.setContent(msg, "text/html");
+				 
+				Multipart multipart = new MimeMultipart();
+				multipart.addBodyPart(mimeBodyPart);
+				 
+				message.setContent(multipart);	
+				
+				try {        
+			         Transport.send(message);
+			         System.out.println("Message envoyé avec succès....");
+					} catch (MessagingException mex) {
+						mex.printStackTrace();
+					}
 				}
-		}
+		
+			} else {
+				System.out.println("Pas de message à envoyer");
+			}
 		
 		}
 
